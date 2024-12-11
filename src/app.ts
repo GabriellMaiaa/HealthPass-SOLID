96300dec724b12ed6d82d8be67d1d55b17c91462
GabriellMaiaa <gribeiromaia736@gmail.com>
2024-12-10 21:10:34 -0400

    04: Adding Prisma ORM to our application

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify";
 //Aqui nesse file app, só criamos a instância do nosso app do Fastify
+import { PrismaClient } from "@prisma/client";
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Gabriel Maia",
+    email: "gmaia@gmail.com",
+  },
+});
